ba36a1c3a10875019e0274078000a57fbd6cd57f
GitHub Action <action@github.com>
2026-08-30 17:04:32 +0000

    chore: update version to 1.3.0

--- aura-preloader.php
+++ aura-preloader.php
@@ -3,7 +3,7 @@
  * Plugin Name: Aura Preloader
  * Plugin URI:  https://wordpress.org/plugins/aura-preloader/
  * Description: A customisable full-screen backdrop-blur preloader with your logo, spinner, and progress bar.
- * Version:     1.2.9
+ * Version:     1.3.0
  * Author:      Irfan Bhat
  * Author URI:  https://irfanbhat.com
  * License:     GPL-2.0+
